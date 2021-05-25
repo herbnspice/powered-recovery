7128b831d9a4ac1d067ed02192745dcce1b307e9
Herbert Saladar <hrbrtsldr@gmail.com>
2021-05-26 05:53:01 +0800

    Firebase integration

---
+++ dist/precache-manifest.2d7b3290247a033c3a6795c337ebc80d.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4408e6f5024ac649a2fd",
+    "url": "/css/app.821a25f9.css"
+  },
+  {
+    "revision": "986602f44131fb6937ecfa83802eef28",
+    "url": "/header.jpeg"
+  },
+  {
+    "revision": "16bafd8616bd346686923650c4b417bd",
+    "url": "/img/Banner.jpg"
+  },
+  {
+    "revision": "13aeba218c31e8ba9b5afdf2bdd52d2b",
+    "url": "/img/Logo.png"
+  },
+  {
+    "revision": "724cf068c555717da45e718dd5112853",
+    "url": "/img/about.jpg"
+  },
+  {
+    "revision": "849deab5d724648dc56ec6326f27c477",
+    "url": "/img/c.jpg"
+  },
+  {
+    "revision": "e637feda967bbd64023f1c2157155f29",
+    "url": "/img/c1.jpg"
+  },
+  {
+    "revision": "d47b2e84236a72d544b42b488e64f32e",
+    "url": "/img/c2.jpg"
+  },
+  {
+    "revision": "3f11584a86a0f8116d12a6d7d5482dc5",
+    "url": "/img/c3.jpg"
+  },
+  {
+    "revision": "b51c64a488198ed4b0aad4e9f04b372a",
+    "url": "/img/gallery1.jpg"
+  },
+  {
+    "revision": "ae9c2498063a6b645faee2a5209c88a1",
+    "url": "/img/gallery2.jpg"
+  },
+  {
+    "revision": "4f1fff1a4a53b6290b8d6a4e26c7d625",
+    "url": "/img/gallery3.jpg"
+  },
+  {
+    "revision": "de7660184f93d99d4f933e5ce5991ae9",
+    "url": "/img/gallery4.jpg"
+  },
+  {
+    "revision": "8fd9661113babc40bb1f8514a3476f05",
+    "url": "/img/gallery5.jpg"
+  },
+  {
+    "revision": "105ab055750843bce8d69c7805f3a90d",
+    "url": "/img/gallery6.jpg"
+  },
+  {
+    "revision": "b611ed833c35cec01a763615f2bfc14d",
+    "url": "/img/gino.png"
+  },
+  {
+    "revision": "363b04e7a5a28b3b7cb2557cad8d4ec3",
+    "url": "/img/programs.jpg"
+  },
+  {
+    "revision": "44aeb9f041b7304fa812c7a253075b27",
+    "url": "/img/review/ajmpareja.jpg"
+  },
+  {
+    "revision": "d24268d20014183839d4d3d26a6ff73b",
+    "url": "/img/review/banzonjansen.jpg"
+  },
+  {
+    "revision": "f4eaee33dec65e2dc6407c6b128a6ee3",
+    "url": "/img/review/joshcraigchabon.jpg"
+  },
+  {
+    "revision": "e42150ce9ff4f3fe693a5bbf6b67e28f",
+    "url": "/img/review/rockytecture.jpg"
+  },
+  {
+    "revision": "f23a4cf789cb8b47608a2921eb5f553a",
+    "url": "/img/review/sobrevinas_kat.jpg"
+  },
+  {
+    "revision": "7f5db51d72a914c126f7ecea321704e4",
+    "url": "/img/review/thekentan.jpg"
+  },
+  {
+    "revision": "87a63347069c2bb964227cd6c4292ce6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b5aeae5274a7be0b372",
+    "url": "/js/about.de5f75d6.js"
+  },
+  {
+    "revision": "4408e6f5024ac649a2fd",
+    "url": "/js/app.05fffa73.js"
+  },
+  {
+    "revision": "4197cdcb8a40960dc356",
+    "url": "/js/chunk-vendors.c31db8ce.js"
+  },
+  {
+    "revision": "d42b8b3ff866660597ed44e166007cdf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBghaTc_YrIUaqC8uY7kmUPK2uc2GZYKNk",
+    authDomain: "poweredrecovery-4c7a2.firebaseapp.com",
+    databaseURL: "https://poweredrecovery-4c7a2-default-rtdb.firebaseio.com",
+    projectId: "poweredrecovery-4c7a2",
+    storageBucket: "poweredrecovery-4c7a2.appspot.com",
+    messagingSenderId: "841548133172",
+    appId: "1:841548133172:web:9c9b0f337308f36d456b61",
+    measurementId: "G-EYYHP2R89J"
+  };
